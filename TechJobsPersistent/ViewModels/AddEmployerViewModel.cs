8a366e3f94edb3adeac9aa153344ea9cb7fa5538
unk1nd0ne <z.neil.schaefer@gmail.com>
2020-07-10 12:41:50 -0500

    Part 1

---
+++ TechJobsPersistent/ViewModels/AddEmployerViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TechJobsPersistent.ViewModels
+{
+    public class AddEmployerViewModel
+    {
+        [Required(ErrorMessage = "Name is required.")]
+        [StringLength(20, MinimumLength = 3, ErrorMessage = "Name must be between 3 and 20 characters")]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage = "Location is required.")]
+        [StringLength(50, MinimumLength = 3, ErrorMessage = "Location must be between 3 and 50 characters")]
+        public string Location { get; set; }
+
+    }
+}
