eaad695e9dc5b54c2aefec8d247bfb3e37c98a29
unk1nd0ne <z.neil.schaefer@gmail.com>
2020-07-13 08:48:10 -0500

    Added sql queries

--- TechJobsPersistent/queries.sql
+++ TechJobsPersistent/queries.sql
@@ -1,6 +1,6 @@
 --Part 1
-
+DESCRIBE jobs;
 --Part 2
-
+SELECT Name FROM employers WHERE Location = "Saint Louis";
 --Part 3
-
+SELECT Name, Description FROM skills RIGHT JOIN jobskills ON skills.Id = jobskills.SkillId;
